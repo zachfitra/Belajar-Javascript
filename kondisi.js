075511612cb1181b8462279519f0d7b7c0f870a5
Ahmad Zaki <ahmadzacky46@gmail.com>
2025-07-10 17:06:28 +0700

    menambah date

--- kondisi.js
+++ kondisi.js
@@ -1,9 +1,13 @@
-let saldoAwal = prompt("Masukan saldo awal");
-let saldoTambahan = prompt("Masukan saldo tambahan");
+// let saldoAwal = prompt("Masukan saldo awal");
+// let saldoTambahan = prompt("Masukan saldo tambahan");
 
-saldoAwal = Number(saldoAwal);
-saldoTambahan = Number(saldoTambahan);
+// saldoAwal = Number(saldoAwal);
+// saldoTambahan = Number(saldoTambahan);
 
-const saldoAkhir = saldoAwal + saldoTambahan;
+// const saldoAkhir = saldoAwal + saldoTambahan;
 
-alert (`Saldo akhir anda adalah ${saldoAkhir}`);
+// alert (`Saldo akhir anda adalah ${saldoAkhir}`);
+
+let hari = new Date () .getDay()
+
+console.log('hari ini adalah hari ke' +hari)

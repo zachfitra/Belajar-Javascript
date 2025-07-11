f50efb762a4853a3ccb139fa635d2ecbebd52c23
Ahmad Zaki <ahmadzacky46@gmail.com>
2025-07-11 14:34:15 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-let nama = 'zaki'
-let usia = 30
-let tinggiBadan = 180
+let nama = prompt ("Masukan nama anda")
+let usia = prompt ("Masukan usia anda")
+let tinggiBadan = prompt ("Masukan Tinggi anda")
 let beratBadan
-let pacar = null
+let pacar = confirm ("Apakah kamu sudah memiliki pacar")
+
+
 
 beratBadan = 92
+beratBadan = prompt ("Masukan berat anda")
+
+if(tinggiBadan <= 100){
+    tinggiBadan = "anda pendek"
+}else {
+    tinggiBadan = "anda tinggi"
+}
 
 if (pacar == null){
     pacar = 'sudah punya pacar'
@@ -13,4 +22,5 @@ if (pacar == null){
 ]
 
 
-alert (`halo nama saya ${nama},usia saya ${usia},tinggi badan saya ${tinggiBadan},berat saya adalah ${beratBadan},dan pacar saya ${pacar}`)
+alert (`halo nama saya ${nama},usia saya ${usia}, ${tinggiBadan},berat saya adalah ${beratBadan},dan pacar saya ${pacar}`)
+console.log (`halo nama saya ${nama},usia saya ${usia}, ${tinggiBadan},berat saya adalah ${beratBadan},dan pacar saya ${pacar}`)

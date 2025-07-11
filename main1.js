1c2e2001526e26ee1ec8856be20dd7c4070ac868
Ahmad Zaki <ahmadzacky46@gmail.com>
2025-07-11 15:21:50 +0700

    menambah main1.js

---
+++ main1.js
@@ -0,0 +1,10 @@
+let nama = prompt ("Masukan nama anda")
+
+if (nama == null || nama.trim() === ''){
+    alert ('kolom tidak boleh kosong');
+}else{
+   alert [`halo ${nama}`]
+ 
+}
+
+alert [`halo ${nama}`]

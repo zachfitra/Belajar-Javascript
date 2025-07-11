c0e78fca6e7e666f5ee30bac7cf8307c5652725b
Ahmad Zaki <ahmadzacky46@gmail.com>
2025-07-11 11:34:29 +0700

    menambahkan file main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+let nama = 'zaki'
+let usia = 30
+let tinggiBadan = 180
+let beratBadan
+let pacar = null
+
+beratBadan = 92
+
+if (pacar == null){
+    pacar = 'sudah punya pacar'
+}else [
+    pacar =  'belum punya pacar'
+]
+
+
+alert (`halo nama saya ${nama},usia saya ${usia},tinggi badan saya ${tinggiBadan},berat saya adalah ${beratBadan},dan pacar saya ${pacar}`)

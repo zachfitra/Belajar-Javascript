df126ba0316bd2abd977f527346cfd376f172814
Ahmad Zaki <ahmadzacky46@gmail.com>
2025-07-14 16:11:31 +0700

    merubah warna button

--- dom.js
+++ dom.js
@@ -1,4 +1,61 @@
-document.title = "Ahmad" 
-const body = document.body
+document.title = ' belajar javascript'
 
-body.append ('Hello world ')
+// const body = document.body
+//  body.append ('Hello world')
+  
+
+
+//  const h1 = document.createElement('h1') //ini adalah variable yang berisi textContent h1
+//  h1.textContent =  '<marquee> ini adalah h1 </marquee>'
+
+// const namaSaya = document.createElement('h2') 
+// namaSaya.innerHTML = '<marquee> Ahmad Zaki Fitra</marquee>'
+
+// const namaKamu = document.createElement('P')
+// namaKamu.innerText = '<marquee>Exela</marquee>'
+
+
+// body.append(h1) // ini adalah perintah untuk menampilkan variable h1 tersebut ke dalam body html
+// body.append(namaKamu)
+// body.append(namaSaya)
+
+ const body = document.body
+ const btn1 = document.getElementById ('btn1')
+ const btn2 = document.getElementById ('btn2')
+ const text2 = document.getElementById('text2')
+
+
+
+const defaultText = 'Klik saya 1'
+const defaultColor = 'green' 
+
+btn1.style.fontFamily = 'arial black'
+btn1.style.background = 'green' 
+
+btn1.textContent = defaultText
+btn1.style.background = defaultColor
+
+
+
+function clickButton(){
+    btn1.style.background = 'aqua'
+    const newText = document.createElement('p')
+    newText.textContent = 'ahmad zaki'   
+    body.append(newText)
+
+
+}
+
+function gantiText(){
+    btn1.textContent = 'saya diklik'
+   
+    }
+function oriText(){
+    btn1.textContent = defaultText
+    btn1.style.background = defaultColor
+}
+
+function ubahWarna (){
+    text2.style.color ='red'
+
+}
